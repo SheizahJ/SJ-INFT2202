d62d00c6fef25e6b95adb6833c0e9a5d7351fe77
SheizahJ <sheizah.jim@gmail.com>
2024-02-08 16:11:38 -0500

    Button Functions + Confirm Message
    HTML
    - Added reset button
    
    CSS
    - Added reset type to button configs
    
    JS
    - Submit/Reset buttons change colour on hover
    - Fade out confirmation message after 3 seconds of showing

--- SJ-ICE-4/script.js
+++ SJ-ICE-4/script.js
@@ -39,10 +39,56 @@ $(document).ready(function () {
                     <p>Thank you, <strong>${name}</strong></p>
                     <p>Your registration for the show on <strong>${date.toUpperCase()}</strong> is confirmed.</p>
                     <p>A confirmation email has been sent to <strong>${email}</strong></p>
-                `).slideDown();
+                `).slideDown().fadeOut(3000);
             });
 
             $("#signup-form").find("input[type=text], input[type=email], select").val("");
         }
     });
+
+    // Handle form reset
+    $("#signup-form input[type=reset]").click(function (event) {
+        $("#confirmation-message").slideUp(function () {
+            $(this).html(`
+                <p>Successfully cleared the form!</p>
+            `).slideDown().fadeOut(3000);
+        });
+    });
+
+    // Form field animations
+    $("input, select").focus(function () {
+        $(this).animate(
+            {
+                borderColor: "#68b3c8",
+            }, 500
+        )
+    }).blur(function () {
+        $(this).animate(
+            {
+                // jQuery.Color is a separate module (optional)
+                borderColor: "#ddd",
+            }, 500
+        )
+    });
+
+    // Submit button animation
+    $("#signup-form input[type=submit]").hover(function () {
+        $(this).animate(
+            {
+                borderColor: "#333333",
+            }, 200
+        )
+    }, function () {
+        $(this).animate(
+            {
+                backgroundColor: "#5c5c5c",
+            }, 200
+        )
+    });
 });
+
+/*  
+    --- ICE 4 TODO ---
+    1. Add a "reset" button that clears all the inputs and the confirmation message when clicked.
+    2. Implement a "fade out" message after a certain period once the registration is confirmed.
+*/
