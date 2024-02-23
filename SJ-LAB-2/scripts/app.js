aab9760458d70f6319959b285b37b5d2e71b7eb0
SheizahJ <sheizah.jim@gmail.com>
2024-02-23 01:10:04 -0500

    Completed Project
    - Added all form validation for Registration page.
    - Added User class for valid registration.
    - Added comments.
    - Fixed any bugs.

--- SJ-LAB-2/scripts/app.js
+++ SJ-LAB-2/scripts/app.js
@@ -5,7 +5,10 @@
  */
 
 // CONSTANTS ----------------------------------------------------------------
-const delayTime = 3000; // in milliseconds (3 seconds)
+const delayTime      = 3000; // in milliseconds (3 seconds)
+const minNameLength  = 2;
+const minEmailLength = 8;
+const minPassLength  = 6;
 
 // PAGES --------------------------------------------------------------------
 const page = window.location.pathname;
@@ -244,6 +247,10 @@ else if (page.includes(contactHref)) {
 }
 
 // LOGIN --------------------------------------------------------------------
+/**
+ * Prepends the provided username before login link.
+ * @param event attributes provided by the form.
+ */
 $(".login-form").submit(function (event) {
     event.preventDefault();
 
@@ -254,10 +261,13 @@ $(".login-form").submit(function (event) {
     let password = $("#password").val().trim();
     
     // error detection
+    // check username for errors
     if (username == "") {
         errors += "Please enter your username.\n";
         isValid = false;
     }
+
+    // check password for errors
     if (password == "") {
         errors += "Please enter your password.\n";
         isValid = false;
@@ -277,12 +287,31 @@ $(".login-form").submit(function (event) {
 
 
 // REGISTRATION -------------------------------------------------------------
+/**
+ * Creates a new user when registring.
+ */
+class User {
+    constructor(firstName, lastName, username, email, password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+/**
+ * Waits for document to be ready before running functions.
+ */
 $(document).ready(function () {
     // create error message div
-    let errorMessageDiv = $("<div hidden id=\"ErrorMessage\">hi</div>");
+    let errorMessageDiv = $("<div hidden id=\"ErrorMessage\"></div>");
     $(".register-form").append(errorMessageDiv);
-    // errorMessageDiv.removeAttr("hidden");
 
+    /**
+     * Attempts to create a new user with the form values.
+     * @param event attributes provided by the form.
+     */
     $(".register-form").submit(function (event) {
         event.preventDefault();
     
@@ -296,40 +325,81 @@ $(document).ready(function () {
         let confirmPass = $("#confirm-pass").val().trim();
     
         // error detection
+        // check first name for errors
         if (firstName == "") {
-            errors += "Please enter your first name.\n";
+            errors += "Please enter your first name.<br/>";
             isValid = false;
         }
+        else if (firstName.length < minNameLength) {
+            errors += "Length of first name must be greater than " + minNameLength + ".<br/>";
+            isValid = false;
+        }
+
+        // check last name for errors
         if (lastName == "") {
-            errors += "Please enter your last name.\n";
+            errors += "Please enter your last name.<br/>";
             isValid = false;
         }
+        else if (lastName.length < minNameLength) {
+            errors += "Length of last name must be greater than " + minNameLength + ".<br/>";
+            isValid = false;
+        }
+
+        // check email for errors
         if (email == "") {
-            errors += "Please enter your email.\n";
+            errors += "Please enter your email.<br/>";
             isValid = false;
         }
+        else if (email.length < minEmailLength) {
+            errors += "Length of email must be greater than " + minEmailLength + ".<br/>";
+            isValid = false;
+        }
+
+        // check password for errors
         if (password == "") {
-            errors += "Please enter your password.\n";
+            errors += "Please enter your password.<br/>";
             isValid = false;
         }
+        else if (password.length < minPassLength) {
+            errors += "Length of password must be greater than " + minPassLength + ".<br/>";
+            isValid = false;
+        }
+
+        // check confirm password for errors
         if (confirmPass == "") {
-            errors += "Please enter your confirmation password.\n";
+            errors += "Please enter your confirmation password.<br/>";
             isValid = false;
         }
         else if (confirmPass != password) {
-            errors += "Passwords do not match.\n";
+            errors += "Passwords do not match.<br/>";
             isValid = false;
         }
     
         // if is valid
         if (isValid) {
-    
+            // create new user
+            const newUser = new User(firstName, lastName, firstName + "." + lastName, email, password);
+            console.log(newUser);
+
+            // reset form
+            $(".register-form")[0].reset();
         }
     
         // if not valid
         else {
-            let navbarInsert = $("<span class=\"navbar-text\"></span>").text(username);
-            alert(errors);
+            // remove hidden attribute
+            errorMessageDiv.removeAttr("hidden");
+
+            // show error
+            errorMessageDiv.slideUp(function () {
+                $(this).html(`
+                    <h4>ERROR</h4>
+                    <p>${errors}</p>
+                `).slideDown().delay(3000).slideUp();
+            });
+
+            // add hidden attribute
+            errorMessageDiv.attr("hidden");
         }
     });
 });
