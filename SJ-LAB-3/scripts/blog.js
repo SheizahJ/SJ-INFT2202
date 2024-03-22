60846f835291f9aceb57e1868a4ff7180df16a31
SheizahJ <sheizah.jim@gmail.com>
2024-03-21 22:48:32 -0400

    Add images to posts
    Add cat images to posts using Pixabay API

--- SJ-LAB-3/scripts/blog.js
+++ SJ-LAB-3/scripts/blog.js
@@ -6,43 +6,62 @@
 
 $(document).ready(async function () {
     const body = $("body")[0];
+    const pixabayKey = "43006455-a08f7b02dec1ceb4a1418f648";
 
+    let images = [];
+    let allBlogPosts = "";
+    
     try {
+        // IMAGES ----------------------------------
+        // fetch pixabay images
+        let pixabayImages = await fetch(
+            `https://pixabay.com/api/?key=${pixabayKey}&q=cat`
+        );
+
+        // get json for images
+        let imagesJson = await pixabayImages.json();
+        console.log(imagesJson.hits);
+
+        imagesJson.hits.forEach(image => {
+            images.push(image.largeImageURL);
+        });
+
+        // BLOG POSTS ------------------------------
         // set pending message
         $("#pending-message")[0].innerHTML = "Fetching posts. Please wait...";
 
         // iterate 20 times (20 blog posts)
-        for (let index = 1; index <= 20; index++) {
+        for (let index = 0; index < 20; index++) {
             // fetch json placeholders
             let jsonPlaceholder = await fetch(
-                `https://jsonplaceholder.typicode.com/posts/${index}`
-            ); // By default, it sends a GET HTTP Request
+                `https://jsonplaceholder.typicode.com/posts/${index+1}`
+            );
 
             // get json results
             let placeholder = await jsonPlaceholder.json();
 
-            let blogHTML = `
+            allBlogPosts += `
             <div class="card blog-container container" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+                <img class="card-img-top" src="${images[index]}" alt="Card image cap">
                 <div class="card-body">
-                    <h6 class="card-subtitle mb-2 text-muted">Blog Post ${index}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Blog Post ${index+1}</h6>
                     <h5 class="card-title">${placeholder.title.toUpperCase()}</h5>
                     <p class="card-text">${placeholder.body}</p>
                     <a href="https://github.com/SheizahJ" class="card-link">GitHub</a>
                     <a href="https://www.linkedin.com/in/sheizah-jimenez-9b1b29265/" class="card-link">LinkedIn</a>
                 </div>
-            </div>
-            <br/>`
+            </div>`
 
-            body.innerHTML += blogHTML;
-
+            
             // if successful, empty pending message
             $("#pending-message")[0].innerHTML = "";
         }
+
+        body.innerHTML += allBlogPosts;
     }
     catch (error) {
         // set error message
-        pendingMessage.innerHTML = "Something went wrong while fetching the posts. Please try again."
+        $("#pending-message")[0].innerHTML = "Something went wrong while fetching the posts. Please try again."
         console.log("Something went wrong... " + error);
     }
 });
