f4304d9b485c1bc3421c80d333ce69c93c23d2d8
SheizahJ <sheizah.jim@gmail.com>
2024-03-28 16:58:47 -0400

    ICE START
    description of ICE

--- FullStack-Pokemon/index.js
+++ FullStack-Pokemon/index.js
@@ -16,3 +16,10 @@ app.use("/", pokemonRouter);
 app.listen(3500, () => {
     console.log("Express server listening on port 3500...");
 });
+
+
+/*  In Class Exercise #7 (DUE: April 2nd)
+
+    As a User, I want to visit localhost:3500/home and see a blank
+    page with the text "Home Page"
+*/
